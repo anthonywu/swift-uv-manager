7787ea933a22cf0f459f8419b50eaa36b29b29c9
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2026-04-18 15:21:26 -0700

    update github link

--- UVManager/Constants.swift
+++ UVManager/Constants.swift
@@ -4,5 +4,5 @@ enum AppConstants {
     static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.0.0"
     static let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "0"
     static let appName = "UV Manager"
-    static let githubURL = "https://github.com/anthonywu"
+    static let githubURL = "https://github.com/anthonywu/swift-uv-manager"
 }
